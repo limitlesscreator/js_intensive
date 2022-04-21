2cdb7509ad7d58ce29bd4c9fc4931fc24254de4d
Vladislav V <0428vlad0428@mail.ru>
2022-04-21 12:34:51 +0300

    homeWork 1

--- homework_1/homeWork_1.js
+++ homework_1/homeWork_1.js
@@ -5,5 +5,40 @@
 // ██║░░██║██████╔╝░░░██║░░░╚█████╔╝██║░╚███║██████╔╝███████╗░░╚██╔╝░░██████╔╝██╗╚█████╔╝╚█████╔╝██║░╚═╝░██║
 // ╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░░╚════╝░╚═╝░░╚══╝╚═════╝░╚══════╝░░░╚═╝░░░╚═════╝░╚═╝░╚════╝░░╚════╝░╚═╝░░░░░╚═╝ :D
 
+//first task
+function convert() {
+    let firstNum = prompt()
+    let secondNum = prompt()
 
+    if (firstNum && secondNum && !isNaN(firstNum) && !isNaN(secondNum)) {
+        let translate = Number(firstNum).toString(Number(secondNum))
+        console.log(translate)
+        return
+    }
+
+    console.log('Некорректный ввод!')
+}
+
+
+//second task
+function calculation() {
+    let firstNum = prompt()
+    let secondNum = prompt()
+
+    if (firstNum && secondNum && !isNaN(firstNum) && !isNaN(secondNum)) {
+        let result = `${Number(firstNum) + Number(secondNum)}, ${firstNum / secondNum}`
+        console.log(result)
+        return
+    }
+
+    console.log('Некорректный ввод!')
+}
+
+// applying functions to buttons
+document.querySelector('#fnc1').addEventListener('click', () => {
+    convert()
+})
+document.querySelector('#fnc2').addEventListener('click', () => {
+    calculation()
+})
 
